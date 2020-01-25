93d73a6f2b0d6524b341f426e0f3290bd5f6bffa
Fhasghrge <1969533391@qq.com>
2020-01-26 00:20:22 +0800

    aside

--- ybdb/src/App.vue
+++ ybdb/src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header>UESTC优秀班集体答辩管理系统</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <Aside></Aside>
+        </el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Copyright @ starstudio</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Aside from './components/aside';
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Aside
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.el-header{
+  background-color: #b3c0d1;
+  color: #333;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  line-height: 60px;
+  font-weight: bolder;
+  letter-spacing: 1em;
+  font-size: 1.4em
+}
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  font-weight: lighter;
+  font-size: .8em
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  /* color: #333; */
+  /* text-align: center; */
+  /* line-height: 200px; */
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
---
+++ ybdb/src/components/aside.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="aside">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#B3C0D1"
+      text-color="#000"
+      active-text-color="#ffd04b"
+    >
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-s-custom"></i>
+          <span>评委信息管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">教师评委信息管理</el-menu-item>
+          <el-menu-item index="1-2">大众评审信息管理</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">班级信息管理</span>
+      </el-menu-item>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-mic"></i>
+          <span>答辩现场管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1">答辩开关控制</el-menu-item>
+          <el-menu-item index="2-2">答辩顺序控制</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">分数管理</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'aside',
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
